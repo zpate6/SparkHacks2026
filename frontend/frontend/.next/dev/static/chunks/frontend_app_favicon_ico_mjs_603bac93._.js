b8bcf0346210e635f365dac0e69c22b08fb80f30
zpate6 <zpate6@uic.edu>
2026-02-07 12:29:06 -0600

    deleted fe folder inside fe folder

--- frontend/frontend/.next/dev/static/chunks/frontend_app_favicon_ico_mjs_603bac93._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/9e883_next_dist_72742c92._.js"
-],
-    source: "dynamic"
-});
