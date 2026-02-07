48336a4cfd579d86544a1c77a3a5ce117dd58e5b
zpate6 <zpate6@uic.edu>
2026-02-07 10:37:38 -0600

    removed backend2

--- backend2/src/main/java/com/SH20266/SH20266/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.SH20266.SH20266.repository;
-
-import com.SH20266.SH20266.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.Optional;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    Optional<User> findByEmail(String email);
-}
