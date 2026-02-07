ef072083d00dc56ae5d555d7f1c5f022d41db1bf
zpate6 <zpate6@uic.edu>
2026-02-06 19:11:45 -0600

    set up backend and frontend

---
+++ backend/src/main/java/com/SparkHacks2026/SparkHacks2026/SparkHacks2026Application.java
@@ -0,0 +1,13 @@
+package com.SparkHacks2026.SparkHacks2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkHacks2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkHacks2026Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/SparkHacks2026/SparkHacks2026/SparkHacks2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SparkHacks2026.SparkHacks2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkHacks2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
